5a8d1959622ec3a08f5e2bbda52109b7cb9ea31e
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-06 19:54:31 +0300

    Добавьте файлы проекта.

---
+++ FormatUtils.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class FormatUtils
+    {
+        public static List<FamilySymbol> GetFormats(Document doc)
+        {
+            var familySymbols = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                .OfClass(typeof(FamilySymbol))
+                .Cast<FamilySymbol>()
+                .ToList();
+            return familySymbols;
+        }
+    }
+}
